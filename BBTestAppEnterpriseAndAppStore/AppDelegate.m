aef133dc88818622e0b5446da77fc39f63983b3f
buddybuild <help@buddybuild.com>
2016-04-22 18:18:07 -0700

    Adding buddybuild SDK
    - Add BuddyBuildSDK.framework
    - Initialize in App Delegate

--- BBTestAppEnterpriseAndAppStore/AppDelegate.m
+++ BBTestAppEnterpriseAndAppStore/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <BuddyBuildSDK/BuddyBuildSDK.h>
 
 @interface AppDelegate ()
 
@@ -16,6 +17,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [BuddyBuildSDK setup];
+    
     // Override point for customization after application launch.
     return YES;
 }
