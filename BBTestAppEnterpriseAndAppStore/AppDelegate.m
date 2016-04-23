06a5e1257d8b9df4f472710fa6aafd58cd10c6cc
Christopher Stott <christopherstott@gmail.com>
2016-04-22 17:58:00 -0700

    Revert "Adding buddybuild SDK"
    This reverts commit a4564f99a8c924df40b156ddd124c2c373cbbc70.

--- BBTestAppEnterpriseAndAppStore/AppDelegate.m
+++ BBTestAppEnterpriseAndAppStore/AppDelegate.m
@@ -7,7 +7,6 @@
 //
 
 #import "AppDelegate.h"
-#import <BuddyBuildSDK/BuddyBuildSDK.h>
 
 @interface AppDelegate ()
 
@@ -17,8 +16,6 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    [BuddyBuildSDK setup];
-    
     // Override point for customization after application launch.
     return YES;
 }
